183a41b9460d86b56862f19cfe2856a4c77cfef2
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2020-11-04 18:55:50 +0530

    Update 4-useState-object.js

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({name:'naruto', age:17, message:'ore wa hokage ni naru'});
+  const changeMessage = () => {
+    if(person.message === 'ore wa hokage ni naru'){
+     setPerson({...person, message:'kore wa ore no nindou da'});
+    }
+    else{
+      setPerson({...person, message:'ore wa hokage ni naru'});
+    }
+  }
+  return <>
+  <h2>{person.name}</h2>
+  <h2>{person.age}</h2>
+  <h3>{person.message}</h3>
+  <button className='btn' onClick={changeMessage}>
+    change message
+  </button>
+  </>
 };
 
 export default UseStateObject;
