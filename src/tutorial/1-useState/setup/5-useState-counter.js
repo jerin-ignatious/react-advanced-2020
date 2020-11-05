4e30e7741307171a5dde36a6a7a81f8561d8bcbb
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2020-11-05 17:26:27 +0530

    Update 5-useState-counter.js

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue((preState) => {
+        return preState+1;
+      });
+    }, 2000);
+  };
   return <>
     <section style={{margin:'4rem 0'}}>
       <h2>Regular Counter</h2>
@@ -9,6 +16,11 @@ const UseStateCounter = () => {
       <button className='btn' onClick={() => setValue(value+1)}>increase</button>
       <button className='btn' onClick={() => setValue(0)}>reset</button>
       <button className='btn' onClick={() => setValue(value-1)}>decrease</button>
+    </section>
+    <section style={{margin:'4rem 0'}}>
+      <h2>complex Counter</h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={complexIncrease}>increase later</button>
     </section>
   </>;
 };
