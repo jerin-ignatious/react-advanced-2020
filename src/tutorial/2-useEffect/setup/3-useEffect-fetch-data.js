1b3f2c2c43cbd8862ad4ec23d5bec6298c88edc1
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2020-11-09 18:50:46 +0530

    Update 3-useEffect-fetch-data.js

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -5,20 +5,27 @@ const url = 'https://api.github.com/users';
 
 
 const UseEffectFetchData = () => {
+  
   const [users, setUsers] = useState([]);
+  
   const getUsers = async() => {
     const response = await fetch(url);
     const users = await response.json();
     setUsers(users);
   }
+  
   useEffect(() => {
     getUsers();
   }, []);
+  
   return( <>
     <h1>github users</h1>
     <ul className='users'>
+      
       {users.map((user)=>{
+        
         const {id, login, avatar_url, html_url} = user;
+        
         return <li key={id}>
           <img src={avatar_url} alt={login}/>
           <div>
