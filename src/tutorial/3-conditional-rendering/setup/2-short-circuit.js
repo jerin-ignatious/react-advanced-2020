045f79d0968f75db309811e2a4118caf921c2e54
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2020-11-13 18:25:46 +0530

    Update 2-short-circuit.js

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,13 +4,17 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('jerin ignatious');
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
   return (
   <>
     <h2>{text || 'john doe'}</h2>
-  <h2>{text && 'hello world'}</h2>
+    <button className='btn' onClick={() => setIsError(!isError)}>
+      toggle error
+    </button>
+  <h2>{isError && 'error..'}</h2>
   </>
   );
 };
